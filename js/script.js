65babaaa7aa72692332e43dba1ba82df548ffe34
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-02 22:13:01 -0300

    criando o laser

--- js/script.js
+++ js/script.js
@@ -37,13 +37,39 @@ function moveDown() {
 
     let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
 
-    if(topPosition === "450px") {
+    if(topPosition === "510px") {
         return
     } else {
         let position = parseInt(topPosition);
         position += 50;
         yourShip.style.top = `${position}px`;
     }
+
+}
+
+// Função de tiro.
+function fireLaser() {
+    
+    let laser = createLaserElement();
+    
+    playArea.appendChild(laser);
+    
+    moveLaser();
+
+}
+
+function createLaserElement() {
+
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    
+    newLaser.scr = '../img/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition - 10}px`;
+    
+    return newLaser;
 
 }
 
